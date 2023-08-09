e3e2160d92eb1263c371d6dcea999f3545724060
MikolajKawalec <mikolaj.kawalec@gmail.com>
2023-08-09 19:17:27 +0200

    more work

--- src/physics_notation.test.ts
+++ src/physics_notation.test.ts
@@ -1,8 +1,25 @@
-import { PhysicsVariable, getEmptyUnit } from './physics_notation';
+import { PhysicsVariable, getEmptyUnit, Unit } from './physics_notation';
 
-import * as mocha from 'mocha';
-import * as chai from 'chai';
+test('creates 0 tuple (array in JS) of size 7', () => {
+	expect(getEmptyUnit()).toMatchObject([0, 0, 0, 0, 0, 0, 0]);
+});
 
-const expect = chai.expect;
+test('Default constructor creates 0ed Physics Variables', () => {
+	expect(new PhysicsVariable().toString()).toEqual('0,0,0,0,0,0,0,0,0');
+});
 
-describe('');
+test('Create 21.37 km (from string)', () => {
+	let pv = PhysicsVariable.fromString('21.37,3,0,1,0,0,0,0,0');
+	expect(pv.toString()).toEqual('21.37,3,0,1,0,0,0,0,0');
+	expect(pv.getValue()).toEqual(21.37);
+	expect(pv.getPrefix()).toEqual(3);
+	expect(pv.getUnit()).toMatchObject([0, 1, 0, 0, 0, 0, 0]);
+});
+
+test('Create 21.37 km (from values)', () => {
+	let pv = PhysicsVariable.fromValues(21.37, 3, [0, 1, 0, 0, 0, 0, 0]);
+	expect(pv.toString()).toEqual('21.37,3,0,1,0,0,0,0,0');
+	expect(pv.getValue()).toEqual(21.37);
+	expect(pv.getPrefix()).toEqual(3);
+	expect(pv.getUnit()).toMatchObject([0, 1, 0, 0, 0, 0, 0]);
+});
--- src/physics_notation.ts
+++ src/physics_notation.ts
@@ -1,8 +1,20 @@
-type Unit<T> = [T, T, T, T, T, T, T];
+import { commonUnits } from './common_units';
+
+export type Unit<T> = [T, T, T, T, T, T, T];
 
 export function getEmptyUnit(): Unit<number> {
 	return [0, 0, 0, 0, 0, 0, 0];
 }
+
+export const Base_Units: Unit<string> = [
+	'time(s)',
+	'length(m)',
+	'mass(kg)',
+	'electric current(A)',
+	'thermodynamic temparature(K)',
+	'amount of substance(mol)',
+	'luminous intensity(cd)',
+];
 
 export class PhysicsVariable {
 	protected value: number;
@@ -40,30 +52,62 @@ export class PhysicsVariable {
 		return pv;
 	}
 
-	public setValue(value: number) {
+	public setValue(value: number): void {
 		this.value = value;
 	}
-	public setPrefix(prefix: number) {
+
+	public setPrefix(prefix: number): void {
 		this.prefix = prefix;
 	}
-	public setUnit(unit: Unit<number>) {
+
+	public setUnit(unit: Unit<number>): void {
 		this.unit = unit;
+	}
+
+	public setUnitByValues(
+		time: number,
+		length: number,
+		mass: number,
+		electric_current: number,
+		thermodynamic_temperature: number,
+		amount_of_substance: number,
+		luminous_intensity: number
+	): void {
+		this.unit[0] = time;
+		this.unit[1] = length;
+		this.unit[2] = mass;
+		this.unit[3] = electric_current;
+		this.unit[4] = thermodynamic_temperature;
+		this.unit[5] = amount_of_substance;
+		this.unit[6] = luminous_intensity;
 	}
 
 	public getValue(): number {
 		return this.value;
 	}
+
 	public getPrefix(): number {
 		return this.prefix;
 	}
+
 	public getUnit(): Unit<number> {
 		return this.unit;
 	}
 
-	public toString() : string {
-        let retStr = this.value.toString() + ",";
-        retStr += this.prefix.toString() + ",";
-        retStr += this.unit.
-        return retStr;
-    }
+	public toString(): string {
+		let retStr = this.value.toString() + ',';
+		retStr += this.prefix.toString() + ',';
+		retStr += this.unit.toString();
+		return retStr;
+	}
+
+	public toVerboseString(bForceSI?: boolean): string {
+		let retStr = this.value.toString();
+		let commonUnit = commonUnits.get(this.unit);
+		console.log(commonUnit);
+		if (bForceSI || !commonUnit) {
+		} else {
+		}
+		return retStr;
+	}
 }
